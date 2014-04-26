83e14301d0619ef470f45925512e75703e70be2b
Felipe Mello <felpmel@gmail.com>
2014-04-26 11:57:08 -0300

    Adicionando a tabela figurinha no banco

--- prototipo/_db/albumdacopa.sql
+++ prototipo/_db/albumdacopa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.7
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 25-Abr-2014 às 16:14
--- Versão do servidor: 5.5.9
--- PHP Version: 5.3.6
+-- Máquina: localhost
+-- Data de Criação: 26-Abr-2014 às 14:55
+-- Versão do servidor: 5.6.12-log
+-- versão do PHP: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,8 +17,24 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `albumdacopa`
+-- Base de Dados: `albumdacopa`
 --
+CREATE DATABASE IF NOT EXISTS `albumdacopa` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `albumdacopa`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `figurinha`
+--
+
+CREATE TABLE IF NOT EXISTS `figurinha` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -29,12 +45,13 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(100) NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
+  `sobrenome` varchar(60) NOT NULL,
   `email` varchar(100) NOT NULL,
   `idade` int(10) NOT NULL,
   `sexo` int(11) NOT NULL,
   `senha` varchar(64) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
