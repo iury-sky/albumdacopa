7626533ed30795bf3bd6b1d68bccfbdd7ba59d16
Felipe Fernandes <ripcorp@gmail.com>
2014-04-25 21:57:51 -0300

    Script cadastro de usuário

--- prototipo/inc/cadastro_usuario.php
+++ prototipo/inc/cadastro_usuario.php
@@ -11,19 +11,15 @@ $senha = $_POST['user_senha'];
 // converte a senha para md5
 $senha_md5 = md5( $senha );
 
-
 //conectando com a tabela do banco de dados
-$database = $database;
+$db = $database;
 $db_table = "usuario";
-$banco = mysql_select_db($database, $conexao); //nome da tabela onde os dados serão armazenados
+$banco = mysql_select_db($db, $conexao); //nome da tabela onde os dados serão armazenados
 
 //Query que realiza a inserção dos dados no banco de dados na tabela indicada acima
-$query = "INSERT INTO `$$db_table` ( `nome` ,`email` ,`idade` ,`sexo` ,`senha` ) 
+$query = "INSERT INTO `$db_table` ( `nome` ,`email` ,`idade` ,`sexo` ,`senha` ) 
 VALUES ( '$nome', '$email', '$idade', '$sexo', '$senha_md5' )";
-//mysql_query($query, $conexao);
 
-echo $query;
-die();
-
+mysql_query($query, $conexao);
 
 ?>
